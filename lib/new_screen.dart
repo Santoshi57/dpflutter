4ad115b698b87e6203b60b0adadf9c00190e474a
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2024-01-24 22:35:00 +0545

    changes

--- lib/new_screen.dart
+++ lib/new_screen.dart
@@ -9,21 +9,68 @@ class NewScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.purple,
         foregroundColor: Colors.white,
-
-        title: Text("New Screen"),
+        centerTitle:true,
       ),
-    /*  body:Center(
-        child: Text("Text")
-      ),
-*/
-      body:Column(
-        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
+    body: Padding(
+      padding:const EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Hello"),
-          Text("World"),
+          Text("Task Title"),
+          TextFormField(
+            decoration: InputDecoration(
+              fillColor:Color(0XFFF5F2F9),
+              filled: true,
+              hintText: "Task Item",
+              focusedBorder: formBorder(true),
+              enabledBorder: formBorder(false),
+
+            ),
+
+          ),
+          const SizedBox(
+            height:8,
+          ),
+          formTitle("Category Name"),
+          const Row(
+            children: [
+              Chip(
+                label: Text("Education",
+                style: TextStyle(
+                  color: Colors.white,
+                ),),
+                backgroundColor: Colors.purple,
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Chip(label: Text("Shop")),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  formTitle("Date"),
+                  SizedBox(
+                    width: 100,
+                    child: TextFormField(),
+                  )
+                ],
+              )
+            ],
+          )
+
+          
         ],
-      )
+      ),
+    ),
     );
   }
+
+  formBorder(bool bool) {}
+
+  formTitle(String s) {}
 }
+
